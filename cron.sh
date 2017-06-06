153c2d883cd7540feaa07878c60f01550a17d0c4
Kznamst <kznamst@gmail.com>
2017-06-06 19:04:30 +0300

    Informations about the path

--- cron.sh
+++ cron.sh
@@ -5,5 +5,5 @@
 
 DATE=$({ hostname; storjshare status | tee >(grep 'running' | awk {'print $(NF-1)'}) >(grep '%' | awk {'print $(NF-1)'}) >/dev/null; } | tr "\n" " ")
 ~/AlertsTelegram/sendtelegram.sh -m " Vps: $DATE"
-
+# You can change here the path /path/to/AlertsTelegram/sendtelegram.sh -m " Vps: $DATE"
 
