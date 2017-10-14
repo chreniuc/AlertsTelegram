4998796c6b5d37ebdef1200e161c7d85aecca3e6
Kznamst <kznamst@gmail.com>
2017-10-15 00:25:20 +0300

    [Fix] Fixed the parsing of the fields for gb usage
    Latest version of storj daemon added multiple fields to the `storjshare status` so i had to update the parsing of the output to get the usage.

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@
 . $HOME/.profile
 . $HOME/.env
 
-DATE=$({ hostname; storjshare status | tee >(grep 'running' | awk {'print $(NF-1)'}) >(grep '%' | awk {'print $(NF-1)'}) >/dev/null; } | tr "\n" " ")
+DATE=$({ hostname; storjshare status | tee >(grep 'running' | awk {'print $(NF-3)'}) >(grep '%' | awk {'print $(NF-2)'}) >/dev/null; } | tr "\n" " ")
 ~/AlertsTelegram/sendtelegram.sh -m " Vps: $DATE"
 # You can change here the path /path/to/AlertsTelegram/sendtelegram.sh -m " Vps: $DATE"
 
